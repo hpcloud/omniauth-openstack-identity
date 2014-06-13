7a181b0ea46c70f49aba39c0c50aba2e726cbef3
Chris Johnson <wchrisjohnson@gmail.com>
2014-06-13 18:13:41 -0400

    initial commit

---
+++ lib/omniauth/openstack/identity.rb
@@ -0,0 +1,9 @@
+require "omniauth/openstack/identity/version"
+
+module Omniauth
+  module Openstack
+    module Identity
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/omniauth/openstack/identity/version.rb
@@ -0,0 +1,7 @@
+module Omniauth
+  module Openstack
+    module Identity
+      VERSION = "0.0.1"
+    end
+  end
+end
