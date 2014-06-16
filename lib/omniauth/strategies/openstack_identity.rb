5a661c07f0272c62a708e75d2affc08e7fd97944
Chris Johnson <wchrisjohnson@gmail.com>
2014-06-16 18:02:37 -0400

    show what's happening

--- lib/omniauth/strategies/openstack_identity.rb
+++ lib/omniauth/strategies/openstack_identity.rb
@@ -1,32 +1,47 @@
 require 'omniauth'
 
-module OmniAuth
-  module Strategies
-    class OpenstackIdentity
-      include OmniAuth::Strategy
+module OmniAuth::Strategies::OpenstackIdentity
+  include OmniAuth::Strategy
 
-      # option :name, "openstack_identity"
-      option :fields, [:username, :password]
-      option :uid_field, :username
-      # option :domain, nil
+  # option :name, "openstack_identity"
+  option :fields, [:username, :password]
+  option :uid_field, :username
+  # option :domain, nil
 
-      # def get_identifier
-      #   f = OmniAuth::Form.new(:title => 'Google Apps Authentication')
-      #   f.label_field('Google Apps Domain', 'domain')
-      #   f.input_field('url', 'domain')
-      #   f.to_response
-      # end
+  # def get_identifier
+  #   f = OmniAuth::Form.new(:title => 'Google Apps Authentication')
+  #   f.label_field('Google Apps Domain', 'domain')
+  #   f.input_field('url', 'domain')
+  #   f.to_response
+  # end
 
-      # def identifier
-      #   options[:domain] || request['domain']
-      # end
-      
-      def request_phase
+  # def identifier
+  #   options[:domain] || request['domain']
+  # end
+
+  def request_phase
+    puts "AUTHORIZE PARAMS"
+    puts authorize_params
+    puts "PARAMS"
+    puts request.params
+    super
+  end
+
+  def authorize_params
+    super.tap do |params|
+      if request.params['scope']
+        params[:scope] = request.params['scope']
       end
-
-      def callback_phase
-      end
-
     end
   end
+
+  # def request_phase
+  #   response = Rack::Response.new
+  #   response.redirect "#{options.authentication_url}?redir=#{full_host + script_name + callback_path}"
+  #   response.finish
+  # end
+
+  def callback_phase
+  end
+
 end
