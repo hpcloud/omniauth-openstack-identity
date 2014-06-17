b4cf166cb342229547ceb260b3d93cf2566f3817
Chris Johnson <wchrisjohnson@gmail.com>
2014-06-17 09:51:26 -0400

    adding puts to understand workflow

--- lib/omniauth/strategies/openstack_identity.rb
+++ lib/omniauth/strategies/openstack_identity.rb
@@ -1,47 +1,60 @@
 require 'omniauth'
 
-module OmniAuth::Strategies::OpenstackIdentity
-  include OmniAuth::Strategy
+module OmniAuth
+  module Strategies
+    class OpenstackIdentity
 
-  # option :name, "openstack_identity"
-  option :fields, [:username, :password]
-  option :uid_field, :username
-  # option :domain, nil
+      include OmniAuth::Strategy
 
-  # def get_identifier
-  #   f = OmniAuth::Form.new(:title => 'Google Apps Authentication')
-  #   f.label_field('Google Apps Domain', 'domain')
-  #   f.input_field('url', 'domain')
-  #   f.to_response
-  # end
+      # option :name, "openstack_identity"
+      option :fields, [:username, :password]
+      option :uid_field, :username
+      # option :domain, nil
 
-  # def identifier
-  #   options[:domain] || request['domain']
-  # end
+      # def get_identifier
+      #   f = OmniAuth::Form.new(:title => 'Google Apps Authentication')
+      #   f.label_field('Google Apps Domain', 'domain')
+      #   f.input_field('url', 'domain')
+      #   f.to_response
+      # end
 
-  def request_phase
-    puts "AUTHORIZE PARAMS"
-    puts authorize_params
-    puts "PARAMS"
-    puts request.params
-    super
-  end
+      # def identifier
+      #   options[:domain] || request['domain']
+      # end
 
-  def authorize_params
-    super.tap do |params|
-      if request.params['scope']
-        params[:scope] = request.params['scope']
+      def setup
+        puts "Entering SETUP phase"
       end
+
+      def request_phase
+        puts "Entering REQUEST phase"
+
+        puts "AUTHORIZE PARAMS"
+        puts authorize_params
+        puts "PARAMS"
+        puts request.params
+        super
+      end
+
+      def authorize_params
+        puts "Entering AUTHORIZE phase"
+
+        super.tap do |params|
+          if request.params['scope']
+            params[:scope] = request.params['scope']
+          end
+        end
+      end
+
+      # def request_phase
+      #   response = Rack::Response.new
+      #   response.redirect "#{options.authentication_url}?redir=#{full_host + script_name + callback_path}"
+      #   response.finish
+      # end
+
+      def callback_phase
+      end
+
     end
   end
-
-  # def request_phase
-  #   response = Rack::Response.new
-  #   response.redirect "#{options.authentication_url}?redir=#{full_host + script_name + callback_path}"
-  #   response.finish
-  # end
-
-  def callback_phase
-  end
-
 end
